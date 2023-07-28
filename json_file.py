37fa15836f4a7401e74270600c65a608d11c47f9
bhim1999 <138559239+bhim1999@users.noreply.github.com>
2023-07-28 13:12:15 +0530

    Create json_file.py

---
+++ json_file.py
@@ -0,0 +1,29 @@
+import json
+# importing the module 
+
+# Employee dictionary created with data written
+dictionary_employees = '''{
+ 'employee_1':{"contactName": "hima","DOB": "17june","height":"4"."city":"bangalore","state":"karnataka"},
+ 'employee_2':{"contactName": "bindu","DOB": "07june","height":"4.4"."city":"bangalore","state":"karnataka"},
+ 'employee_3':{"contactName": "maduri","DOB": "18june","height":"6"."city":"bangalore","state":"karnataka"},
+ 'employee_4':{"contactname": "swarna","DOB": "12july","height":"5.2"."city":"bangalore","state":"karnataka"},
+ 'employee_5':{"contactName": "divya","DOB": "23jan","height":"4.6"."city":"bangalore","state":"karnataka"},
+}'''
+
+
+# Printing the dictionary data
+print("The dictionary is as: \n", dictionary_employees, "\n")
+
+output:
+The dictionary is as: 
+ {
+ 'employee_1':{"contactName": "hima","DOB": "17june","height":"4"."city":"bangalore","state":"karnataka"},
+ 'employee_2':{"contactName": "bindu","DOB": "07june","height":"4.4"."city":"bangalore","state":"karnataka"},
+ 'employee_3':{"contactName": "maduri","DOB": "18june","height":"6"."city":"bangalore","state":"karnataka"},
+ 'employee_4':{"contactname": "swarna","DOB": "12july","height":"5.2"."city":"bangalore","state":"karnataka"},
+ 'employee_5':{"contactName": "divya","DOB": "23jan","height":"4.6"."city":"bangalore","state":"karnataka"},
+}
+
+
+
+
