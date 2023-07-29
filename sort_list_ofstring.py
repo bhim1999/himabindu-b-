99d3e076daa6e96936450ce5700fb324465d0d1d
bhim1999 <138559239+bhim1999@users.noreply.github.com>
2023-07-29 11:50:11 +0530

    Create sort_list_ofstring.py

---
+++ sort_list_ofstring.py
@@ -0,0 +1,16 @@
+# Python program to sort a list of strings
+
+lst = ['edyoda', 'is', 'a', 'portal', 'for', 'bindu']
+
+# Using sorted() function
+for ele in sorted(lst):
+	print(ele)
+
+output:
+PS C:\Users\HIMA BINDU\Documents\Python VSC> python -u "e:\Practice\sort_listofstring.py"
+a
+bindu
+edyoda
+for
+is
+portal
